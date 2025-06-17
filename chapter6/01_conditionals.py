5e6002f2443e7414a5d51261ee8e54c6090c5922
iamsabbiralam <sabbiriph4@gmail.com>
2025-06-17 13:41:23 +0600

    [tupc] - practice set
    1. practice set

---
+++ chapter6/01_conditionals.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: "))
+if (a > 18):
+      print("You are above the age of consent")
+elif (a <= 0):
+      print("You have entered an invalid age")
+else:
+      print("You are below the age of consent")
+
+print("end of program")
